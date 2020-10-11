ce4bb3626a8a6019afa105964482b83676c12e25
Kent Holsinger <keholsinger@gmail.com>
2020-10-11 10:52:12 -0400

    Remove doc directory from .gitignore

---
+++ doc/Hickory.R
@@ -0,0 +1,49 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup, echo = FALSE------------------------------------------------------
+library(Hickory)
+library(readr)
+
+## From https://roxygen2.r-lib.org/articles/rd-formatting.html
+##
+tabular <- function(df, ...) {
+  stopifnot(is.data.frame(df))
+
+  align <- function(x) if (is.numeric(x)) "r" else "l"
+  col_align <- purrr::map_chr(df, align)
+
+  cols <- lapply(df, format, ...)
+  contents <- do.call("paste",
+    c(cols, list(sep = " \\tab ", collapse = "\\cr\n#'   ")))
+
+  paste("#' \\tabular{", paste(col_align, collapse = ""), "}{\n#'   ",
+    paste0("\\strong{", names(df), "}", sep = "", collapse = " \\tab "), " \\cr\n#'   ",
+    contents, "\n#' }\n", sep = "")
+}
+
+## ---- echo = FALSE------------------------------------------------------------
+dat <- read_csv(system.file("extdata", "protea_repens.csv", package = "Hickory"),
+                col_types = cols())
+knitr::kable(dat[c(1:5, 25:30), 1:10])
+
+## ---- echo = FALSE------------------------------------------------------------
+dat <- read_csv(system.file("extdata", "dominant_example.csv", package = "Hickory"),
+                col_types = cols())
+knitr::kable(dat[c(1:5, 25:30), 1:10])
+
+## -----------------------------------------------------------------------------
+options(mc.cores = parallel::detectCores())
+
+genos <- read_marker_data(system.file("extdata", "protea_repens.csv", package = "Hickory"))
+fit_free <- analyze_codominant(genos)
+
+## -----------------------------------------------------------------------------
+options(mc.cores = parallel::detectCores())
+
+genos <- read_marker_data(system.file("extdata", "dominant_example.csv", package = "Hickory"))
+fit_free <- analyze_dominant(genos)
+
