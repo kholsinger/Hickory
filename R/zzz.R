0a3ce8b37a26ffa03647da6e592e23b869d6c69b
Kent Holsinger <keholsinger@gmail.com>
2022-01-08 17:39:06 -0500

    Using environment to hide package variables
    Preliminary test suggests that it's working. Need to run all analysis routines in clean environment to check.

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,14 @@
+HickoryEnv <- new.env()
+
 .onLoad <- function(libname, pkgname) {
   ## set default priors
   ##
   ## Note:: These will be overwritten by defaults in analyze_*()
   ##
-  prior_pi <<- list(lower = 0.1, upper = 0.9)
-  prior_f <<- list(lower = 0.01, upper = 0.2)
-  prior_theta <<- list(lower = 0.01, upper = 0.2)
-  prior_alpha <<- 1
-  N_loci <<- 5
-  N_pops <<- 10
+  assign("prior_pi", list(lower = 0.1, upper = 0.9), envir = HickoryEnv)
+  assign("prior_f", list(lower = 0.01, upper = 0.2), envir = HickoryEnv)
+  assign("prior_theta", list(lower = 0.01, upper = 0.2), envir = HickoryEnv)
+  assign("prior_alpha", 1, envir = HickoryEnv)
+  assign("N_loci", 5, envir = HickoryEnv)
+  assign("N_pops", 10, envir = HickoryEnv)
 }
